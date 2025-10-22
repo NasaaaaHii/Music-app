f1e2973b8815d88ce8c8f76db49d73f31daab94b
cuong9565 <cuongmanhle0905@gmail.com>
2025-10-22 11:13:24 +0700

    Cuong push library

--- app/(tabs)/library.tsx
+++ app/(tabs)/library.tsx
@@ -1,10 +1,26 @@
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Ionicons } from "@expo/vector-icons";
+import { ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function library(){
-    return (
-        <SafeAreaView className='flex justify-center items-center bg-red-200 w-full h-full'>
-            <Text>library</Text>
-        </SafeAreaView>
-    );
+export default function library() {
+  return (
+    <SafeAreaView className="flex">
+      <ScrollView
+        contentContainerClassName="w-full h-full  bg-[#f4f3f8]"
+        showsVerticalScrollIndicator={false}
+      >
+        <View className="flex flex-col justify-center items-center gap-3 w-full p-10">
+          <Ionicons
+            name="library-outline"
+            size={50}
+            color={"#fff"}
+            borderRadius={100}
+            className="bg-sky-500 p-10"
+          />
+          <Text className="text-3xl font-bold text-gray-900">Thư viện</Text>
+          <Text className="text-gray-600">3 danh sách phát</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
