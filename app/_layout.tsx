1ec55101882e6aeec788b04feb5a0df5c9eb652b
cuong9565 <cuongmanhle0905@gmail.com>
2025-10-29 10:29:50 +0700

    Cuong fix conflig

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,17 +3,10 @@ import "react-native-reanimated";
 import "./global.css";
 
 export default function RootLayout() {
-<<<<<<< HEAD
   return (
     <Stack initialRouteName="(tabs)" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" />
       <Stack.Screen name="modal" />
     </Stack>
   );
-=======
-  return <Stack screenOptions={{ headerShown: false }} >
-    <Stack.Screen name="index" />
-    <Stack.Screen name="modal" />
-  </Stack>
->>>>>>> 16bab324c152aac380ad0aa834bc73c225450a16
 }
