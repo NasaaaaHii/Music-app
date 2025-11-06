f36352368e22084f9061f2a5e4fb44be8d63c1aa
NHanh <nha261105@gmail.com>
2025-11-06 21:42:06 +0700

    may thang 10 wind

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,64 +1,103 @@
-import { Feather, Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
 import { Tabs } from "expo-router";
-import { UserRound } from "lucide-react-native";
-import { View } from "react-native";
-import MiniPlayerSong from "../Components/MiniPlayerSong";
+import { House, Library, Search, UserRound } from "lucide-react-native";
+import { StyleSheet, View } from "react-native";
 import { t } from "../theme";
+
 export default function _layout() {
   return (
     <View className="flex-1" style={{ backgroundColor: t.surface }}>
       <Tabs
+        initialRouteName="home"
         screenOptions={{
           headerShown: false,
-          tabBarActiveTintColor: "#00bc7d",
-          tabBarInactiveTintColor: "#4a5565",
+          tabBarShowLabel: false,
           tabBarStyle: {
-            height: 56.5,
-            backgroundColor: t.tabBarBg,
-            borderTopColor: t.tabBarBorder,
-            borderTopWidth: 1,
+            position: "absolute",
+            bottom: 20,
+            height: 40,
+            width: 350,
+            borderRadius: 35,
+            borderTopWidth: 0,
+            elevation: 5,
+            shadowColor: "#00ff99",
+            shadowOpacity: 0.15,
+            shadowOffset: { width: 0, height: 3 },
+            shadowRadius: 12,
+            overflow: "hidden",
+            backgroundColor: "transparent",
+            alignSelf: "center",
+            alignItems: "center",
           },
+          tabBarBackground: () => (
+            <LinearGradient
+              colors={[
+                "rgba(60, 60, 60, 0.95)", // trên
+                "rgba(25, 25, 25, 0.95)", // giữa
+                "rgba(10, 10, 10, 0.95)", // dưới
+              ]}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 0, y: 1 }}
+              style={{
+                ...StyleSheet.absoluteFillObject,
+                borderRadius: 35,
+              }}
+            />
+          ),
         }}
-        initialRouteName="home"
       >
         <Tabs.Screen
           name="home"
           options={{
-            title: "Trang chủ",
-            tabBarIcon: ({ size, color }) => (
-              <Feather name="home" size={24} color={color} />
+            tabBarIcon: ({ focused }) => (
+              <View
+                className={`rounded-full ${focused ? "bg-[#C7E845] p-2" : "bg-transparent"}`}
+              >
+                <House size={24} color={focused ? "#1A1A1A" : "#B0B0B0"} />
+              </View>
             ),
           }}
         />
+
         <Tabs.Screen
           name="search"
           options={{
-            title: "Tìm kiếm",
-            tabBarIcon: ({ color }) => (
-              <Feather name="search" size={24} color={color} />
+            tabBarIcon: ({ focused }) => (
+              <View
+                className={`rounded-full ${focused ? "bg-[#C7E845] p-2" : "bg-transparent"}`}
+              >
+                <Search size={24} color={focused ? "#1A1A1A" : "#B0B0B0"} />
+              </View>
             ),
           }}
         />
+
         <Tabs.Screen
           name="library"
           options={{
-            title: "Thư viện",
-            tabBarIcon: ({ color }) => (
-              <Ionicons name="library-outline" size={24} color={color} />
+            tabBarIcon: ({ focused }) => (
+              <View
+                className={`rounded-full ${focused ? "bg-[#C7E845] p-2" : "bg-transparent"}`}
+              >
+                <Library size={24} color={focused ? "#1A1A1A" : "#B0B0B0"} />
+              </View>
             ),
           }}
         />
+
         <Tabs.Screen
           name="account"
           options={{
-            title: "Tài khoản",
-            tabBarIcon: ({ color }) => <UserRound size={24} color={color} />,
+            tabBarIcon: ({ focused }) => (
+              <View
+                className={`rounded-full ${focused ? "bg-[#C7E845] p-2" : "bg-transparent"}`}
+              >
+                <UserRound size={24} color={focused ? "#1A1A1A" : "#B0B0B0"} />
+              </View>
+            ),
           }}
         />
       </Tabs>
-      <View className="absolute bottom-6 left-0 right-0 px-4">
-        <MiniPlayerSong />
-      </View>
     </View>
   );
 }
